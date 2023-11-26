c00693d0b1dfb0df3be18f08e0c902f0c5fae86f
Mateo Meza <mezamateoj@gmail.com>
2023-11-26 20:23:29 -0300

    Add new files and update dependencies

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,29 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+
+import CabinTable from '../features/cabins/CabinTable';
+import { useState } from 'react';
+import Button from '../ui/Button';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+	const [showForm, setShowForm] = useState(false);
+	return (
+		<>
+			<Row type="horizontal">
+				<Heading as="h1">All cabins</Heading>
+				<p>Filter / sort</p>
+			</Row>
+			<Row>
+				<CabinTable />
+
+				<Button onClick={() => setShowForm(!showForm)}>
+					Add new Cabin
+				</Button>
+				{showForm && <CreateCabinForm />}
+			</Row>
+		</>
+	);
 }
 
 export default Cabins;
