2e58f579065f22b7825b0de0478b52e8d58b82d7
Mateo Meza <mezamateoj@gmail.com>
2023-11-26 20:22:32 -0300

    Add new UI components

---
+++ src/ui/Input.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+	border: 1px solid var(--color-gray-300);
+	background-color: var(--color-gray-0);
+	border-radius: var(--border-radius-sm);
+`;
+
+export default Input;
